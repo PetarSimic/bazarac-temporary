a3fbc1b00d292dcfd43d0a83a4cebb84f059814c
PetarS <simicp87@gmail.com>
2016-08-09 17:21:07 +0200

    header (osnovno), new arrivals (osnovno)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,80 @@
-/**
- * Created by Mirijevcani on 8/9/2016.
- */
+$(document).ready(function(){
+
+    //fake checkbox
+    $(".fake-checkbox .checkbox").on("change", function(){
+        $(this).siblings("label").toggleClass("active");
+    });
+
+    //custom dropdown
+    function customDropdown(){
+        $(".drop-trigger").on("click", function(){
+            if($(this).hasClass("open")){
+                $(this).closest(".drop-holder").find(".drop-content-holder").removeClass("open").slideUp();
+                $(this).removeClass("open");
+            }else{
+                $(".drop-content-holder").removeClass("open").slideUp();
+                $(".drop-trigger").removeClass("open");
+                $(this).closest(".drop-holder").find(".drop-content-holder").addClass("open").slideDown();
+                $(this).addClass("open");
+            }
+        });
+    }
+    customDropdown();
+
+    //custom dropdown close on click outside
+    $(document).click(function(e) {
+        if (!($(e.target).is(".drop-trigger, .drop-trigger *, .drop-content-holder, .drop-content-holder *"))) {
+            $(".drop-content-holder").slideUp().removeClass("open");
+            $(".drop-trigger").removeClass("open");
+        }
+    });
+
+
+    $('.main-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        speed: 1000,
+        //speed: 500,
+        //fade: true,
+        //cssEase: 'linear',
+        pauseOnHover: false,
+        pauseOnFocus: false
+    });
+
+
+    $('.slick-type2').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+});
---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+
+    <title>Bazarac</title>
+
+    <!-- fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600,400,300' rel='stylesheet' type='text/css'>
+
+    <!-- CSS FILES -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- JS FILES -->
+    <script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+
+
+</head>
+<body>
+
+<header>
+
+
+    <section class="header-top">
+
+        <div class="container">
+
+            <!--logo-->
+            <a class="logo" href="#">
+                <img src="assets/images/logo.png" alt="Bazarac - logo" />
+            </a>
+
+            <!--search-->
+            <form class="search-form">
+                <input type="text" placeholder="Search Bazarac..." />
+                <button type="submit"><span class="fa fa-search"></span></button>
+            </form>
+
+            <div class="right-block">
+
+                <a class="sell-link" href="#">Sell on Bazarac</a>
+
+                <div class="drop-holder">
+                    <a href="#" class="drop-trigger">Sign In</a>
+                    <div class="drop-content-holder">
+                        <div class="drop-content">
+                            aaa
+                        </div>
+                    </div>
+                </div>
+
+                <div class="drop-holder currency-select">
+                    <a href="#" class="drop-trigger">RSD</a>
+                    <div class="drop-content-holder">
+                        <div class="drop-content">
+                            <ul>
+                                <li><a href="#">RSD</a></li>
+                                <li><a href="#">USD</a></li>
+                                <li><a href="#">EUR</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="drop-holder language-select">
+                    <a href="#" class="drop-trigger">EN</a>
+                    <div class="drop-content-holder">
+                        <div class="drop-content">
+                            <ul>
+                                <li><a href="#">SR</a></li>
+                                <li><a href="#">EN</a></li>
+                                <li><a href="#">DE</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="drop-holder shopping-cart">
+                    <a href="#" class="drop-trigger"><span class="fa fa-shopping-cart"></span></a>
+                    <div class="drop-content-holder">
+                        <div class="drop-content">
+                            Your cart is empty
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+    </section>
+
+    <section class="header-middle">
+
+        <div class="main-slider cover-bg" style="background-image: url('assets/images/temporary/slider1.jpg')">
+            <div class="item">
+                <div class="container relative">
+                    <div class="position-helper">
+                        <div class="slide-content">
+                            <img src="assets/images/temporary/slider-studio1.jpg" />
+                            <div class="text">
+                                <h3>PropelerStudio</h3>
+                                <div class="desc">Pottery is addictive and our little project is a good example of this.</div>
+                                <a class="link" href="#">VISIT SHOP <span class="fa fa-angle-double-right"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="container relative">
+                    <div class="position-helper">
+                        <div class="slide-content">
+                            <img src="assets/images/temporary/slider-studio1.jpg" />
+                            <div class="text">
+                                <h3>PropelerStudio2</h3>
+                                <div class="desc">Pottery is addictive and our little project is a good example of this.</div>
+                                <a class="link" href="#">VISIT SHOP <span class="fa fa-angle-double-right"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="header-bottom">
+        <nav class="container">
+            <ul class="main-navigation">
+                <li><a href="#">Clothing & Accessories</a></li>
+                <li><a href="#">Jewelry</a></li>
+                <li><a href="#">Craft Supplies & Tools</a></li>
+                <li><a href="#">Home & Living</a></li>
+                <li><a href="#">Entertainment</a></li>
+                <li><a href="#">Weddings</a></li>
+                <li><a href="#">Vintage</a></li>
+                <li><a href="#">Kids&Babies</a></li>
+            </ul>
+        </nav>
+    </section>
+
+
+    <section class="new-arrivals">
+
+        <div class="container">
+
+            <div class="slider-pos-helper">
+                <div class="slick-type2">
+                    <div class="item">
+                        <a href="#">
+                            <img src="assets/images/temporary/eyewear-w-1.jpg">
+                        </a>
+                        <div class="text">
+                            <h3><a>Te pri sint iudico. Alia pericula ne mel, et minim option sea, ne inani abhorreant consequuntur ius.</a></h3>
+                            <div class="bottom-block">
+                                <h4>Rambag</h4>
+                                <div class="price-holder"><span class="currency">EUR</span> <span class="price">120.00</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <img src="assets/images/temporary/eyewear-w-2.jpg">
+                        </a>
+                        <div class="text">
+                            <h3><a>Te pri sint iudico. Alia pericula ne mel, et minim option sea, ne inani abhorreant consequuntur ius.</a></h3>
+                            <div class="bottom-block">
+                                <h4>Rambag</h4>
+                                <div class="price-holder"><span class="currency">EUR</span> <span class="price">120.00</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <img src="assets/images/temporary/eyewear-w-3.jpg">
+                        </a>
+                        <div class="text">
+                            <h3><a>Te pri sint iudico. Alia pericula ne mel, et minim option sea, ne inani abhorreant consequuntur ius.</a></h3>
+                            <div class="bottom-block">
+                                <h4>Rambag</h4>
+                                <div class="price-holder"><span class="currency">EUR</span> <span class="price">120.00</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <img src="assets/images/temporary/eyewear-w-4.jpg">
+                        </a>
+                        <div class="text">
+                            <h3><a>Te pri sint iudico. Alia pericula ne mel, et minim option sea, ne inani abhorreant consequuntur ius.</a></h3>
+                            <div class="bottom-block">
+                                <h4>Rambag</h4>
+                                <div class="price-holder"><span class="currency">EUR</span> <span class="price">120.00</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <img src="assets/images/temporary/eyewear-w-5.jpg">
+                        </a>
+                        <div class="text">
+                            <h3><a>Te pri sint iudico. Alia pericula ne mel, et minim option sea, ne inani abhorreant consequuntur ius.</a></h3>
+                            <div class="bottom-block">
+                                <h4>Rambag</h4>
+                                <div class="price-holder"><span class="currency">EUR</span> <span class="price">120.00</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <img src="assets/images/temporary/eyewear-w-6.jpg">
+                        </a>
+                        <div class="text">
+                            <h3><a>Te pri sint iudico. Alia pericula ne mel, et minim option sea, ne inani abhorreant consequuntur ius.</a></h3>
+                            <div class="bottom-block">
+                                <h4>Rambag</h4>
+                                <div class="price-holder"><span class="currency">EUR</span> <span class="price">120.00</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <img src="assets/images/temporary/eyewear-w-7.jpg">
+                        </a>
+                        <div class="text">
+                            <h3><a>Te pri sint iudico. Alia pericula ne mel, et minim option sea, ne inani abhorreant consequuntur ius.</a></h3>
+                            <div class="bottom-block">
+                                <h4>Rambag</h4>
+                                <div class="price-holder"><span class="currency">EUR</span> <span class="price">120.00</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+</header>
+
+<script src="assets/bower_components/slick-carousel/slick/slick.js"></script>
+<script src="assets/js/theme.js"></script>
+
+</body>
+</html>
